2e2bb3a180c8e0fed140497167feb9c86e8f79ec
temaors <artsiom.orsik@gmail.com>
2022-11-23 20:27:26 +0300

    Fixed inserting date to seedData Task

--- DIMS-Core.Database/DIMS-Core.Database/SeedData/TasksSeedData.sql
+++ DIMS-Core.Database/DIMS-Core.Database/SeedData/TasksSeedData.sql
@@ -7,25 +7,25 @@ INSERT INTO [dbo].[Tasks]
 )
 VALUES (
     'Database basic',
-    'Implement 2 tables: Tasks, UserProfiles'
-    2022-01-01,
-    2022-02-02
+    'Implement 2 tables: Tasks, UserProfiles',
+    '2022-01-01',
+    '2022-02-02'
     ),
     (
     'Advanced database',
-    'Add indexes to the created tables'
-    2022-02-03,
-    2022-03-03
+    'Add indexes to the created tables',
+    '2022-02-03',
+    '2022-03-03'
     ),
     (
     'Implement repositories',
-    'Implement 2 repositories for entities(Tasks, UserProfiles)'
-    2022-03-04,
-    2022-04-04
+    'Implement 2 repositories for entities(Tasks, UserProfiles)',
+    '2022-03-04',
+    '2022-04-04'
     ),
     (
     'Implement controllers',
-    'Implement 2 controllers: TasksController, UserProfilesController'
-    2022-04-05,
-    2022-05-05
+    'Implement 2 controllers: TasksController, UserProfilesController',
+    '2022-04-05',
+    '2022-05-05'
     );
