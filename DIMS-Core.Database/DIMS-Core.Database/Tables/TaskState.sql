0c5b094891215b96d032609f59fcacb86b7615e2
Nektarinchik <ermolen2003@gmail.com>
2022-11-24 21:01:37 +0300

    added 'TaskState' and 'UserTask' tables

---
+++ DIMS-Core.Database/DIMS-Core.Database/Tables/TaskState.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[TaskState]
+(
+	[StateId]   INT          NOT NULL IDENTITY (1, 1),
+	[StateName] NVARCHAR(40) NOT NULL
+
+	CONSTRAINT PK_TaskState_StateId PRIMARY KEY (StateId)
+)
---
+++ DIMS-Core.Database/DIMS-Core.Database/Tables/UserTask.sql
@@ -0,0 +1,11 @@
+﻿CREATE TABLE [dbo].[UserTask]
+(
+	[UserTaskId] INT NOT NULL IDENTITY(1, 1),
+	[TaskId]     INT NOT NULL,
+	[UserId]     INT NOT NULL,
+	[StateId]    INT NOT NULL,
+
+	CONSTRAINT PK_UserTask_UserTaskId PRIMARY KEY (UserTaskId),
+	CONSTRAINT FK_UserTask_TaskState_StateId FOREIGN KEY (StateId) REFERENCES TaskState (StateId)
+		ON DELETE CASCADE
+)
