eb62ce72e5182489236f5c80d8a057a1e97831f7
Nektarinchik <ermolen2003@gmail.com>
2022-11-24 21:34:55 +0300

    added SetUserTaskAsSuccess procedure

---
+++ DIMS-Core.Database/DIMS-Core.Database/Procedures/SetUserTaskAsSuccess.sql
@@ -0,0 +1,15 @@
+﻿CREATE PROCEDURE [dbo].[SetUserTaskAsSuccess]
+	@UserId int,
+	@TaskId int
+AS
+BEGIN
+	UPDATE [dbo].[UserTask]
+	SET [StateId] = (
+		SELECT DISTINCT [StateId]
+		FROM [dbo].[TaskState]
+		WHERE [StateName] = 'Success'
+	)
+	WHERE   [UserId] = @UserId
+		AND [TaskId] = @TaskId
+END
+
