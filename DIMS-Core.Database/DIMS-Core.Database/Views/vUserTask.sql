d535c4d24505625c6d10eece230bef6196b23f36
Nektarinchik <ermolen2003@gmail.com>
2022-11-24 21:22:59 +0300

    replaced base table in query that create vUserTask view

--- DIMS-Core.Database/DIMS-Core.Database/Views/vUserTask.sql
+++ DIMS-Core.Database/DIMS-Core.Database/Views/vUserTask.sql
@@ -1,14 +1,13 @@
 ﻿CREATE VIEW [dbo].[vUserTask]
 	AS
-SELECT [UserProfiles].UserId,
+SELECT  [UserTask].UserId,
 		[Tasks].TaskId,
 		[Tasks].Name,
 		[Tasks].Description,
 		[Tasks].StartDate,
 		[Tasks].DeadlineDate,
 		[TaskState].StateName
-FROM [dbo].[UserTask] 
-	INNER JOIN [dbo].[UserProfiles] ON [dbo].[UserTask].[UserId] = [dbo].[UserProfiles].[UserId]
-	INNER JOIN [dbo].[Tasks]        ON [dbo].[UserTask].[TaskId] = [dbo].[Tasks].[TaskId]
-	INNER JOIN [dbo].[TaskState]    ON [dbo].[UserTask].[StateId] = [dbo].[TaskState].[StateId]
+FROM [dbo].[Tasks] 
+	INNER JOIN [dbo].[UserTask]  ON [dbo].[UserTask].[TaskId] = [dbo].[Tasks].[TaskId]
+	INNER JOIN [dbo].[TaskState] ON [dbo].[UserTask].[StateId] = [dbo].[TaskState].[StateId]
 
