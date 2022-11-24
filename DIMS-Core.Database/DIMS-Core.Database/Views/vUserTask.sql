b50c091f102d6b12ca0097b3848b09cd0fe930ba
Nektarinchik <ermolen2003@gmail.com>
2022-11-24 21:18:27 +0300

    added view vUserTask

---
+++ DIMS-Core.Database/DIMS-Core.Database/Views/vUserTask.sql
@@ -0,0 +1,14 @@
+﻿CREATE VIEW [dbo].[vUserTask]
+	AS
+SELECT [UserProfiles].UserId,
+		[Tasks].TaskId,
+		[Tasks].Name,
+		[Tasks].Description,
+		[Tasks].StartDate,
+		[Tasks].DeadlineDate,
+		[TaskState].StateName
+FROM [dbo].[UserTask] 
+	INNER JOIN [dbo].[UserProfiles] ON [dbo].[UserTask].[UserId] = [dbo].[UserProfiles].[UserId]
+	INNER JOIN [dbo].[Tasks]        ON [dbo].[UserTask].[TaskId] = [dbo].[Tasks].[TaskId]
+	INNER JOIN [dbo].[TaskState]    ON [dbo].[UserTask].[StateId] = [dbo].[TaskState].[StateId]
+
