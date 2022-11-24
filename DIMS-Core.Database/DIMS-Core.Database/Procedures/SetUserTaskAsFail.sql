f1dcd49d1cbbe0ee861208ee607a06bb132d83f7
Nektarinchik <ermolen2003@gmail.com>
2022-11-24 21:38:35 +0300

    added SetUserTaskAsFail procedure

---
+++ DIMS-Core.Database/DIMS-Core.Database/Procedures/SetUserTaskAsFail.sql
@@ -0,0 +1,14 @@
+﻿CREATE PROCEDURE [dbo].[SetUserTaskAsFail]
+	@UserId int,
+	@TaskId int
+AS
+BEGIN
+	UPDATE [dbo].[UserTask]
+	SET [StateId] = (
+		SELECT DISTINCT [StateId]
+		FROM [dbo].[TaskState]
+		WHERE [StateName] = 'Fail'
+	)
+	WHERE   [UserId] = @UserId
+		AND [TaskId] = @TaskId
+END
