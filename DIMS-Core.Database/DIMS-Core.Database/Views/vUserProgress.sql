475d2b22f8be0430f13a059616dca85c985a57f0
Nektarinchik <ermolen2003@gmail.com>
2022-12-02 17:14:21 +0300

    added necessary views vUserProgress and vUserTrack

---
+++ DIMS-Core.Database/DIMS-Core.Database/Views/vUserProgress.sql
@@ -0,0 +1,17 @@
+﻿CREATE VIEW [dbo].[vUserProgress]
+	AS
+	SELECT  [dbo].[UserTask].[UserId]       AS UserId,
+			[dbo].[UserTask].[TaskId]       AS TaskId,
+			[dbo].[TaskTrack].[TaskTrackId] AS TaskTrackId,
+			CONCAT(
+				[dbo].[UserProfiles].[FirstName], 
+				' ', 
+				[dbo].[UserProfiles].[LastName]
+			)                               AS UserName,
+			[dbo].[Tasks].[Name]            AS TaskName,
+			[dbo].[TaskTrack].[TrackNote]   AS TrackNote,
+			[dbo].[TaskTrack].[TrackDate]   AS TrackDate
+	FROM [dbo].[UserTask] 
+		INNER JOIN [dbo].[TaskTrack]    ON [dbo].[TaskTrack].[UserTaskId] = [dbo].[UserTask].[UserTaskId]
+		INNER JOIN [dbo].[UserProfiles] ON [dbo].[UserTask].[UserId] = [dbo].[UserProfiles].[UserId]
+		INNER JOIN [dbo].[Tasks]        ON [dbo].[UserTask].[TaskId] = [dbo].[Tasks].[TaskId]
---
+++ DIMS-Core.Database/DIMS-Core.Database/Views/vUserTrack.sql
@@ -0,0 +1,11 @@
+﻿CREATE VIEW [dbo].[vUserTrack]
+	AS
+	SELECT  [dbo].[UserTask].[UserId]       AS UserId,
+			[dbo].[UserTask].[TaskId]       AS TaskId,
+			[dbo].[TaskTrack].[TaskTrackId] AS TaskTrackId,
+			[dbo].[Tasks].[Name]            AS TaskName,
+			[dbo].[TaskTrack].[TrackNote]   AS TrackNote,
+			[dbo].[TaskTrack].[TrackDate]   AS TrackDate
+	FROM [dbo].[UserTask]
+		INNER JOIN [dbo].[TaskTrack] ON [dbo].[UserTask].[UserTaskId] = [dbo].[TaskTrack].[UserTaskId]
+		INNER JOIN [dbo].[Tasks]     ON [dbo].[UserTask].[TaskId] = [dbo].[Tasks].[TaskId]
