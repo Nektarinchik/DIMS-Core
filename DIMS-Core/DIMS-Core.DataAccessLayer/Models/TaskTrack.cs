f536df6ca9b12f9ae6e0d504c65f00bdd2e360d9
Nektarinchik <ermolen2003@gmail.com>
2022-12-02 18:30:27 +0300

    added entities files

---
+++ DIMS-Core/DIMS-Core.DataAccessLayer/Models/TaskTrack.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DIMS_Core.DataAccessLayer.Models.temp
+{
+    internal class TaskTrack
+    {
+    }
+}
