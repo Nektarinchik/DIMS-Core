9bf9dcf98953a6e451e672aba9304a8fddc7ed5b
Nektarinchik <ermolen2003@gmail.com>
2022-12-18 16:15:09 +0300

    remove async in IUserTaskRepository

--- DIMS-Core/DIMS-Core.DataAccessLayer/Interfaces/IUserTaskRepository.cs
+++ DIMS-Core/DIMS-Core.DataAccessLayer/Interfaces/IUserTaskRepository.cs
@@ -11,7 +11,7 @@ namespace DIMS_Core.DataAccessLayer.Interfaces
 {
     public interface IUserTaskRepository : IRepository<UserTask>
     {
-        public Task SetUserTaskAsFail(int userId, int taskId);
-        public Task SetUserTaskAsSuccess(int userId, int taskId);
+        public void SetUserTaskAsFail(int userId, int taskId);
+        public void SetUserTaskAsSuccess(int userId, int taskId);
     }
 }
