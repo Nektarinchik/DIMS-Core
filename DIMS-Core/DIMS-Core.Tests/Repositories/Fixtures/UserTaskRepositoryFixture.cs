ad8456313eb168607722fe7ab836a2d3f6b3119a
Nektarinchik <ermolen2003@gmail.com>
2022-12-18 16:13:41 +0300

    added UserTaskFixture class

---
+++ DIMS-Core/DIMS-Core.Tests/Repositories/Fixtures/UserTaskRepositoryFixture.cs
@@ -0,0 +1,15 @@
+using DIMS_Core.DataAccessLayer.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DIMS_Core.Tests.Repositories.Fixtures
+{
+    internal class UserTaskRepositoryFixture
+    {
+        public DimsCoreContext Context { get; set; }
+        public IR MyProperty { get; set; }
+    }
+}
